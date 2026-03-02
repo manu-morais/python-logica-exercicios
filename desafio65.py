84dfb35eda02addf4571a5ed0fe531038fb15c01
Manuella Morais <145507501+manu-morais@users.noreply.github.com>
2026-03-02 20:14:35 -0300

    feat : criei desafio65.py

---
+++ desafio65.py
@@ -0,0 +1,16 @@
+numeros = []
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in numeros:
+        numeros.append(n)
+        print('Valor adicionado com sucesso...')
+    else:
+        print('Valor duplicado! Não vou adicionar...')
+    r = str(input('Quer continuar? [S/N] ')).strip().upper()
+    if r in 'N':
+        break
+print('-=' * 30)
+numeros.sort()
+print(f'Você digitou os valores {numeros}')
+
+     
